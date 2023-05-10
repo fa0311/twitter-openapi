31a2103cd0621260e06129ef8fba73db78dfb607
ふぁ <yuki@yuki0311.com>
2023-05-10 11:49:18 +0900

    update config
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- tools/build_config.py
+++ tools/build_config.py
@@ -49,16 +49,23 @@ class Config:
                         ReplaceQueryIdPlaceholder(split=-1),
                         AddSecuritySchemesOnHeader(split=-1),
                         SetResponsesHeader(suffix=None),
-                        AddParametersOnParametersAsObject(split=-1),
+                        AddParametersOnBody(split=-1),
                     ]
                     for key in ["post"]
                 }
                 | {
                     key: [
                         SetResponsesHeader(suffix="legacy"),
-                        AddParametersOnParametersAsObject(split=2),
+                        AddParametersOnParametersAsString(split=2),
                     ]
-                    for key in ["v1.1-get", "v1.1-post", "v2.0-get", "v2.0-post"]
+                    for key in ["v1.1-get", "v2.0-get"]
+                }
+                | {
+                    key: [
+                        SetResponsesHeader(suffix="legacy"),
+                        AddParametersOnBody(split=2),
+                    ]
+                    for key in ["v1.1-post", "v2.0-post"]
                 },
             },
             "typescript": {
