ab2b0ba115f373bf2d4627c8a66dde185ff6a1f5
ふぁ <yuki@yuki0311.com>
2023-06-14 18:04:59 +0900

    update config
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- tools/build_config.py
+++ tools/build_config.py
@@ -5,196 +5,73 @@ class Config:
     OUTPUT_DIR = "dist/{0}"
     INPUT_DIR = "src/openapi"
 
+    def hooks_generator(self, queryParameterJson=True):
+        # https://stackoverflow.com/questions/34820064/defining-an-api-with-swagger-get-call-that-uses-json-in-parameters/45223964
+        if queryParameterJson:
+            getParamHook = AddParametersOnContent(
+                split=-1,
+                contentType="application/json",
+                ignoreKeys=["queryId"],
+            )
+            postParamHook = AddParametersOnParameters(
+                split=-1,
+                schemaType=None,
+            )
+        else:
+            getParamHook = AddParametersOnParameters(
+                split=-1,
+                schemaType="string",
+                ignoreKeys=["queryId"],
+            )
+            postParamHook = AddParametersOnBody(
+                split=-1,
+                schemaType=None,
+                contentType="application/json",
+            )
+
+        return {
+            "openapi": [AddSecuritySchemesOnSecuritySchemes()],
+            "schemas": [],
+            "other": [],
+            "request": {
+                key: [
+                    SetResponsesHeader(suffix=None),
+                    AddPathQueryIdOnParameters(split=-1),
+                    getParamHook,
+                ]
+                for key in ["default", "user", "user-list", "tweet"]
+            }
+            | {
+                key: [
+                    SetResponsesHeader(suffix=None),
+                    AddPathQueryIdOnParameters(split=-1),
+                    postParamHook,
+                ]
+                for key in ["post"]
+            }
+            | {
+                key: [
+                    SetResponsesHeader(suffix="legacy"),
+                    AddParametersOnParameters(split=2, schemaType=None),
+                ]
+                for key in ["v1.1-get", "v2.0-get"]
+            }
+            | {
+                key: [
+                    SetResponsesHeader(suffix="legacy"),
+                    AddParametersOnBody(
+                        split=2,
+                        schemaType=None,
+                        contentType="application/x-www-form-urlencoded",
+                    ),
+                ]
+                for key in ["v1.1-post", "v2.0-post"]
+            }
+            | {"other": []},
+        }
+
     def main(self):
         return {
-            "docs": {
-                "openapi": [AddSecuritySchemesOnSecuritySchemes()],
-                "schemas": [],
-                "other": [],
-                "request": {
-                    key: [
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnContent(
-                            split=-1, contentType="application/json"
-                        ),
-                    ]
-                    for key in ["default", "user", "user-list", "tweet"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnParameters(
-                            split=-1,
-                            schemaType=None,
-                        ),
-                    ]
-                    for key in ["post"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix="legacy"),
-                        AddParametersOnParameters(
-                            split=2,
-                            schemaType=None,
-                        ),
-                    ]
-                    for key in ["v1.1-get", "v2.0-get"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix="legacy"),
-                        AddParametersOnBody(
-                            split=2,
-                            schemaType=None,
-                            contentType="application/x-www-form-urlencoded",
-                        ),
-                    ]
-                    for key in ["v1.1-post", "v2.0-post"]
-                }
-                | {"other": []},
-            },
-            "dart": {
-                "openapi": [],
-                "schemas": [],
-                "other": [],
-                "request": {
-                    key: [
-                        AddSecuritySchemesOnHeader(split=-1),
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnParameters(
-                            split=-1,
-                            schemaType="string",
-                        ),
-                    ]
-                    for key in ["default", "user", "user-list", "tweet"]
-                }
-                | {
-                    key: [
-                        AddSecuritySchemesOnHeader(split=-1),
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnBody(
-                            split=-1,
-                            schemaType=None,
-                            contentType="application/json",
-                        ),
-                    ]
-                    for key in ["post"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix="legacy"),
-                        AddSecuritySchemesOnHeader(split=-1),
-                        AddParametersOnParameters(
-                            split=2,
-                            schemaType=None,
-                        ),
-                    ]
-                    for key in ["v1.1-get", "v2.0-get"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix="legacy"),
-                        AddSecuritySchemesOnHeader(split=-1),
-                        AddParametersOnBody(
-                            split=2,
-                            schemaType=None,
-                            contentType="application/x-www-form-urlencoded",
-                        ),
-                    ]
-                    for key in ["v1.1-post", "v2.0-post"]
-                }
-                | {"other": []},
-            },
-            "typescript": {
-                "openapi": [AddSecuritySchemesOnSecuritySchemes()],
-                "schemas": [],
-                "other": [],
-                "request": {
-                    key: [
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnParameters(
-                            split=-1,
-                            schemaType="string",
-                        ),
-                    ]
-                    for key in ["default", "user", "user-list", "tweet"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnBody(
-                            split=-1,
-                            schemaType=None,
-                            contentType="application/json",
-                        ),
-                    ]
-                    for key in ["post"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix="legacy"),
-                        AddParametersOnParameters(
-                            split=2,
-                            schemaType=None,
-                        ),
-                    ]
-                    for key in ["v1.1-get", "v2.0-get"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix="legacy"),
-                        AddParametersOnBody(
-                            split=2,
-                            schemaType=None,
-                            contentType="application/x-www-form-urlencoded",
-                        ),
-                    ]
-                    for key in ["v1.1-post", "v2.0-post"]
-                }
-                | {"other": []},
-            },
-            "test": {
-                "openapi": [AddSecuritySchemesOnSecuritySchemes()],
-                "schemas": [],
-                "other": [],
-                "request": {
-                    key: [
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnParameters(
-                            split=-1,
-                            schemaType="string",
-                        ),
-                    ]
-                    for key in ["default", "user", "user-list", "tweet"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix=None),
-                        AddPathQueryIdOnParameters(split=-1),
-                        AddParametersOnParameters(
-                            split=-1,
-                            schemaType="string",
-                        ),
-                    ]
-                    for key in ["post"]
-                }
-                | {
-                    key: [
-                        SetResponsesHeader(suffix="legacy"),
-                        AddParametersOnParameters(
-                            split=2,
-                            schemaType="object",
-                        ),
-                    ]
-                    for key in ["v1.1-get", "v1.1-post", "v2.0-get", "v2.0-post"]
-                }
-                | {"other": []},
-            },
+            "docs": self.hooks_generator(),
+            "compatible": self.hooks_generator(queryParameterJson=False),
         }
